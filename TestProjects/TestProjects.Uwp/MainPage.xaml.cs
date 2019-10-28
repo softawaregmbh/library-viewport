45ab5a0444ec19facd1a4955c55e63b8b5218ba5
Daniel Sklenitzka <daniel.sklenitzka@softaware.at>
2019-10-28 11:17:34 +0100

    Added test projects

---
+++ TestProjects/TestProjects.Uwp/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using softaware.ViewPort.Core;
+using softaware.ViewPort.UserInteraction;
+using softaware.ViewPort.Uwp.UserInteraction;
+using softaware.ViewPort.WinRT;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Resources.Core;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace TestProjects.Uwp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            IUIThread uiThread = new UwpUIThread();
+            IUserInteractionProvider userInteractionProvider = new UwpUserInteractionProvider();
+
+            Task.Run(async () =>
+            {
+                await Task.Delay(1000);
+                await uiThread.RunAsync(async () =>
+                {
+                    await userInteractionProvider.GetUserInputAsync("Test", "English button labels", UserInputOption.YesNoCancel, UserInputResult.Cancel);
+                    ResourceContext.SetGlobalQualifierValue("Language", "de-AT");
+                    await userInteractionProvider.GetUserInputAsync("Test", "German button labels", UserInputOption.YesNoCancel, UserInputResult.Cancel);
+                });
+            });
+        }
+    }
+}
---
+++ TestProjects/TestProjects.Wpf.Core/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using softaware.ViewPort.Core;
+using softaware.ViewPort.UserInteraction;
+using softaware.ViewPort.Wpf;
+using softaware.ViewPort.Wpf.UserInteraction;
+using System.Globalization;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TestProjects.Wpf.Core
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            IUIThread uiThread = new WpfUIThread();
+            IUserInteractionProvider userInteractionProvider = new WpfUserInteractionProvider();
+
+            Task.Run(async () =>
+            {
+                await Task.Delay(1000);
+                await uiThread.RunAsync(async () =>
+                {
+                    CultureInfo.CurrentUICulture = new CultureInfo("en-US");
+                    await userInteractionProvider.GetFolderAsync();
+
+                    CultureInfo.CurrentUICulture = new CultureInfo("de-AT");
+                    await userInteractionProvider.GetFolderAsync();
+                });
+            });
+        }
+    }
+}
